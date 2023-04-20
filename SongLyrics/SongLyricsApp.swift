a76a500920ce8b23a13d66392dde42b675090a8f
Justin Hui <jhui@lcs.on.ca>
2023-04-20 11:16:56 -0400

    Initial Commit

---
+++ SongLyrics/SongLyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongLyricsApp.swift
+//  SongLyrics
+//
+//  Created by Justin Hui on 20/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct SongLyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
